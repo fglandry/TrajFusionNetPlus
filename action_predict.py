16e36218ea21beb6a2570853f4f008bc735b3daf
Francois Landry <efl7126@umoncton.ca>
2026-03-21 15:19:49 -0400

    Add

--- action_predict.py
+++ action_predict.py
@@ -1536,8 +1536,7 @@ class ActionPredict(object):
                               generator=self._generator, 
                               complete_data=complete_data,
                               dataset_name=model_opts["dataset_full"],
-                              test_only=test_only,
-                              prev_hf_logging_level=prev_hf_logging_level)
+                              test_only=test_only)
             
         with open(os.path.join(model_path, 'configs.yaml'), 'r') as fid:
             opts = yaml.safe_load(fid)
